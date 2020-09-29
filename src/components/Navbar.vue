a6f09a3d16df763596bfb200cf1c553e0b1ee2e4
Deniz Çağın Demirci <denizcdemirci@gmail.com>
2020-09-30 02:48:09 +0300

    cache switch added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,18 @@
 <template>
-  <nav class="navbar navbar-light bg-light mb-4">
+  <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
     <div class="container">
       <router-link to="/" class="navbar-brand">Homepage</router-link>
-      <a class="nav-link" href="https://github.com/denizcdemirci/cacheable">GitHub</a>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="https://github.com/denizcdemirci/cacheable">GitHub Repo</a>
+          </li>
+        </ul>
+      </div>
+      <div class="custom-switch">
+        <input v-model="cache" type="checkbox" class="custom-control-input" id="cacheSwitch">
+        <label class="custom-control-label" for="cacheSwitch">Toggle cache</label>
+      </div>
     </div>
   </nav>
 </template>
@@ -10,5 +20,19 @@
 <script>
 export default {
   name: 'Navbar',
+  data() {
+    return {
+      cache: true,
+    };
+  },
+  watch: {
+    cache(value) {
+      if (value) {
+        window.location.reload();
+      } else {
+        this.$axios.defaults.adapter = null;
+      }
+    },
+  },
 };
 </script>
